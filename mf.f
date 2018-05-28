3c477c921fa2e7dc100620135b6f67cc22d5c12d
Iruatã Souza <iru.muzgo@gmail.com>
2018-05-28 10:20:41 -0700

    Initial commit

---
+++ mf.f
@@ -0,0 +1,28 @@
+variable reg-a
+variable reg-b
+
+: a!  reg-a ! ;
+: a   reg-a @ ;
+: b!  reg-b ! ;
+: b   reg-b @ ;
+
+: @b  b @ ;
+: !b  b ! ;
+
+: @+   a @  a cell + a! ;
+: !+   a !  a cell + a! ;
+: c@+  a c@  a 1+ a! ;
+: c!+  a c!  a 1+ a! ;
+
+: push  postpone >r ; immediate
+: pop   postpone r> ; immediate
+
+: for
+  postpone begin postpone dup
+  postpone while postpone push ; immediate
+
+: next
+  postpone pop    postpone 1-
+  postpone repeat postpone drop ; immediate
+
+: if  postpone dup postpone if ; immediate
